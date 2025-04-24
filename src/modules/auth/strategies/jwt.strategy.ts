f22ad9d921ab43410ddb2f70a317f53dc0bf93c0
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-04-24 16:39:50 -0300

    feat: implements jwt authentication for accessTokens generation and passport strategy

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET_KEY'),
+    });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.authService.findUserById(payload.sub);
+
+    if (!user) {
+      throw new UnauthorizedException('user not found');
+    }
+
+    return {
+      userId: payload.sub,
+      email: payload.email,
+    };
+  }
+}
