a931a8461d3b2868f4e335371870ddaac3bc87ba
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-04-25 18:28:01 -0300

    feat: update imports on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { NfcCaptureModule } from './modules/nfc_caputre/nfc-capture.module';
+import { MovementsModule } from './modules/movements/movements.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     NfcCaptureModule,
+    MovementsModule,
   ],
   controllers: [],
   providers: [],
