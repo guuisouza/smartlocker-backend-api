baf57660740c5c90c9d34e2eb9d596346bb2dca2
guuisouza <thekingguiiz2003@gmail.com>
2025-04-26 11:53:46 -0300

    style: remove comments on movements service

--- src/modules/movements/movements.service.ts
+++ src/modules/movements/movements.service.ts
@@ -1,49 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { format } from 'date-fns';
-
-/* 
-Quando o NFC é capturado:
-
-    🔍 Obter notebook associado à tag NFC (nfc_tag → notebook) OK
-
-    🧭 Obter o armário onde esse notebook está alocado (notebook → cabinet_id) OK 
-
-    🏫 Obter a sala do armário (cabinet → room_id) OK 
-
-    📅 Obter a aula (schedule) atual na sala no momento da captura (room_id + datetime) OK
-
-    📦 Verificar se o notebook está sendo retirado ou devolvido
-
-    📝 Registrar a movimentação na tabela movements
-*/
 
 @Injectable()
 export class MovementsService {
   constructor(private readonly prisma: PrismaService) {}
 
   async handleNfcMovement(nfc_tag: string, datetime: string) {
-    // 1. Pega o notebook pela tag
     const notebook = await this.prisma.notebooks.findFirst({
       where: { nfc_tag },
     });
-    if (!notebook) throw new Error('Notebook não encontrado.');
-
+    if (!notebook) throw new NotFoundException('notebook not found.');
     const notebook_id = notebook.id;
 
-    // 2. Pega o cabinet_id
     const cabinet = await this.prisma.cabinets.findFirst({
       where: { notebooks: { some: { id: notebook_id } } },
     });
-    if (!cabinet) throw new Error('Cabinet não encontrado.');
-
-    // const cabinet_id = cabinet.id;
-
-    // 3. Pega o room_id
+    if (!cabinet) throw new NotFoundException('cabinet not found.');
     const room_id = cabinet.room_id;
 
-    // 4. Busca a schedule que está ativa naquela data/hora e sala
-    // Dias da semana com acento correto, para bater com os valores do banco
     const weekdays = [
       'Domingo',
       'Segunda',
@@ -58,9 +33,8 @@ export class MovementsService {
     const dayOfWeekIndex = captureDate.getDay();
     const formattedDay = weekdays[dayOfWeekIndex];
 
-    const timeOnly = format(captureDate, 'HH:mm:ss'); // Ex: "21:57:35"
+    const timeOnly = format(captureDate, 'HH:mm:ss');
 
-    // Busca a schedule com SQL puro
     const scheduleResult = await this.prisma.$queryRaw<
       Array<{ id: number }>
     >`SELECT id FROM schedules
@@ -71,12 +45,12 @@ export class MovementsService {
     LIMIT 1`;
 
     if (!scheduleResult.length) {
-      throw new Error('Nenhuma aula agendada para essa sala nesse horário.');
+      throw new NotFoundException(
+        'no classes scheduled for this room at this time.',
+      );
     }
-
     const schedule_id = scheduleResult[0].id;
 
-    // 5. Verifica se já houve uma retirada sem devolução
     const existingMovement = await this.prisma.movements.findFirst({
       where: {
         notebook_id,
@@ -89,15 +63,12 @@ export class MovementsService {
       },
     });
 
-    // 6. Agora define se é retirada ou devolução
     if (!existingMovement) {
-      // RETIRADA
       await this.prisma.$executeRaw`
         INSERT INTO movements (notebook_id, checkout_datetime, schedule_id, room_id)
         VALUES (${notebook_id}, ${datetime}, ${schedule_id}, ${room_id})
       `;
     } else {
-      // DEVOLUÇÃO
       await this.prisma.$executeRaw`
         UPDATE movements
         SET return_datetime = ${datetime}
